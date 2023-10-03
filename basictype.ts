e28042607e3d3251d526f3b3340dab814c305a65
Vaidik_187 <vaidikbhesaniya1212@gmail.com>
2023-10-03 22:35:12 +0530

    day02

---
+++ basictype.ts
@@ -0,0 +1,23 @@
+//basic type
+//string
+let company:string='vaidik bhesaniya';
+console.log(company);
+
+//boolean 
+let issundet:boolean=true
+console.log(issundet);
+
+//any
+
+let x:any="hello"
+console.log(x);
+
+//number
+
+
+let Num:number=5;
+console.log(Num);
+
+
+
+
--- intro.ts
+++ intro.ts
@@ -1,2 +1,4 @@
 let a:number=4;
 console.log(a);
+let id:number=12;
+console.log(id);
